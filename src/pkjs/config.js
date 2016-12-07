fbfc7b66ed736ff45ec8d4e2c84a8934e033ad40
Jake <jacob.rusch@gmail.com>
2016-12-06 19:22:37 -0600

    Code for 1.0

---
+++ src/pkjs/config.js
@@ -0,0 +1,35 @@
+module.exports = [
+	{ 
+    	"type": "heading", 
+    	"defaultValue": "Settings",
+		  "size": 1
+	},
+	{
+		"type": "section",
+		"items": [
+			{
+				"type": "heading",
+				"defaultValue": "Color Selection"
+			},
+			{
+				"type": "toggle",
+				"messageKey": "KEY_INVERT_COLORS",
+				"label": "Invert Colors",
+				"defaultValue": false
+			}
+		]
+	},
+	{
+		"type": "submit",
+		"defaultValue": "Apply Settings"
+	},
+	{
+		"type": "text",
+		"defaultValue": "Version 1.0"
+	},
+  {
+    "type": "text",
+    "defaultValue": "<a href='http://darksky.net/'>Powered by Dark Sky</a>"
+  }
+];
+
